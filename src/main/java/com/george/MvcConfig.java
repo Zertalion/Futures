08cd7f50f07db32f45680cdb3d20b21576665625
George Nedov <George Nedov>
2016-05-18 16:02:13 +0300

    login working(without database)

---
+++ src/main/java/com/george/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.george;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/index").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+       // registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("/login");
+    }
+
+}
---
+++ src/main/java/com/george/model/Users.java
@@ -0,0 +1,24 @@
+package com.george.model;
+
+import lombok.*;
+
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+
+
+public class Users {
+
+    private String name;
+    private String password;
+
+    public Users(){
+
+    }
+}
+
